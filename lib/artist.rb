668df44d9af2dbce987f856f46641798e305412e
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-07-23 12:58:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,29 @@
-require 'pry'
-
 class Artist
-  @@all = []
+  attr_accessor :name
+  attr_reader :songs
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
 
- attr_accessor :name
+  @@artists = []
 
- def initialize(name)
-   @name = name
- end
+  def initialize
+    super
+    @songs = []
+  end
 
- def self.all
-   @@all
- end
+  def self.all
+    @@artists
+  end
 
- def self.find_by_name(name)
-   @@all.detect {|a| a.name == name}
- end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
+
 end
